f807fe1d86eebbad462bd15d220ec1077ca19285
GauravGupta131220 <grvgupta12@gmail.com>
2017-08-22 16:14:54 +0530

    education code optimised

--- education.js
+++ education.js
@@ -31,117 +31,35 @@ var myReadStream = fs.readFile(filePath, 'utf8', function(err, data) {
         }
         result.push(obj);
     }
-    //console.log(result);
 
-    var without_education = result.reduce((c, ele) => {
+    var educationData = [];
+    var cateogaryArray = ['Educational level - Literate without educational level - Persons', 'Educational level - Below Primary - Persons', 'Educational level - Primary - Persons', 'Educational level - Middle - Persons', 'Educational level - Matric/Secondary - Persons', 'Educational level - Higher secondary/Intermediate/Pre-University/Senior secondary - Persons', 'Educational level - Non-technical diploma or certificate not equal to degree - Persons', 'Educational level - Technical diploma or certificate not equal to degree - Persons', 'Educational level - Graduate & above - Persons', 'Educational level - Unclassified - Persons'];
+    var cateogarySum = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.withoutEducation);
-        return c;
-    }, 0);
-
-    var below_primary = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.belowPrimary);
-        return c;
-    }, 0);
-
-    var primary = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.primary);
-        return c;
-    }, 0);
-
-    var middle = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.middle);
-        return c;
-    }, 0);
-
-    var matric_secondary = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.MatricSecondary);
-        return c;
-    }, 0);
-
-    var higher_secondary = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.higherSecondary);
-        return c;
-    }, 0);
-
-    var non_technical_diploma = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.nonTechnicalDiploma);
-        return c;
-    }, 0);
-
-    var technical_diploma = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.technicalDiploma);
-        return c;
-    }, 0);
-
-    var graduate_and_above = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.graduateAndAbove);
-        return c;
-    }, 0);
-
-    var unclassified = result.reduce((c, ele) => {
-
-        if ((ele.total == 'Total') && (ele.age == 'All ages'))
-            c = c + parseInt(ele.unclassified);
-        return c;
-    }, 0);
-
-
-    //console.log(JSON.stringify(without_education, null, 2));
-
-    var myObj = {
-        educationData: []
-    };
-
-
-    myObj.educationData.push({ withoutEducation: without_education });
-    myObj.educationData.push({ belowPrimary: below_primary });
-    myObj.educationData.push({ primary: primary });
-    myObj.educationData.push({ middle: middle });
-    myObj.educationData.push({ MatricSecondary: matric_secondary });
-    myObj.educationData.push({ HigherSecondary: higher_secondary });
-    myObj.educationData.push({ NonTechnicalDiploma: non_technical_diploma });
-    myObj.educationData.push({ TechnicalDiploma: technical_diploma });
-    myObj.educationData.push({ GraduateAndAbove: graduate_and_above });
-    myObj.educationData.push({ Unclassified: unclassified });
-
-    var withoutEducation = JSON.stringify(myObj, null, 2);
-    var belowPrimEducation = JSON.stringify(myObj, null, 2);
-    var primEducation = JSON.stringify(myObj, null, 2);
-    var middleEducation = JSON.stringify(myObj, null, 2);
-    var matricEducation = JSON.stringify(myObj, null, 2);
-    var higherSecEducation = JSON.stringify(myObj, null, 2);
-    var nonTechDipEducation = JSON.stringify(myObj, null, 2);
-    var techDiplomaEducation = JSON.stringify(myObj, null, 2);
-    var graduateandAboveEdu = JSON.stringify(myObj, null, 2);
-    var unclassifiedEducation = JSON.stringify(myObj, null, 2);
-
-    fs.writeFile('educationCateogary.json', withoutEducation);
-    fs.writeFile('educationCateogary.json', belowPrimEducation);
-    fs.writeFile('educationCateogary.json', primEducation);
-    fs.writeFile('educationCateogary.json', middleEducation);
-    fs.writeFile('educationCateogary.json', matricEducation);
-    fs.writeFile('educationCateogary.json', higherSecEducation);
-    fs.writeFile('educationCateogary.json', nonTechDipEducation);
-    fs.writeFile('educationCateogary.json', techDiplomaEducation);
-    fs.writeFile('educationCateogary.json', graduateandAboveEdu);
-    fs.writeFile('educationCateogary.json', unclassifiedEducation);
-
+    for (let i = 0; i < result.length; i++) {
+        for (let j = 0; j < cateogaryArray.length; j++) {
+            if ((result[i].total == 'Total') && (result[i].age == 'All ages')) {
+                if (cateogaryArray[j] == 'Educational level - Literate without educational level - Persons') cateogarySum[j] += parseFloat(result[i].withoutEducation)
+                if (cateogaryArray[j] == 'Educational level - Below Primary - Persons') cateogarySum[j] += parseFloat(result[i].belowPrimary)
+                if (cateogaryArray[j] == 'Educational level - Primary - Persons') cateogarySum[j] += parseFloat(result[i].primary)
+                if (cateogaryArray[j] == 'Educational level - Middle - Persons') cateogarySum[j] += parseFloat(result[i].middle)
+                if (cateogaryArray[j] == 'Educational level - Matric/Secondary - Persons') cateogarySum[j] += parseFloat(result[i].MatricSecondary)
+                if (cateogaryArray[j] == 'Educational level - Higher secondary/Intermediate/Pre-University/Senior secondary - Persons') cateogarySum[j] += parseFloat(result[i].higherSecondary)
+                if (cateogaryArray[j] == 'Educational level - Non-technical diploma or certificate not equal to degree - Persons') cateogarySum[j] += parseFloat(result[i].nonTechnicalDiploma)
+                if (cateogaryArray[j] == 'Educational level - Technical diploma or certificate not equal to degree - Persons') cateogarySum[j] += parseFloat(result[i].technicalDiploma)
+                if (cateogaryArray[j] == 'Educational level - Graduate & above - Persons') cateogarySum[j] += parseFloat(result[i].graduateAndAbove)
+                if (cateogaryArray[j] == 'Educational level - Unclassified - Persons') cateogarySum[j] += parseFloat(result[i].unclassified)
+            }
+        }
+    }
+    for (let j = 0; j < cateogaryArray.length; j++) {
+        obj = {
+            cateogary: cateogaryArray[j],
+            cateogarySum: cateogarySum[j]
+        }
+        educationData.push(obj)
+    }
+    var outPath = path.join(__dirname, 'educationCateogary.json');
+    fs.writeFile(outPath, JSON.stringify(educationData, null, 2), 'utf8',
+        function(err) { console.log(err); });
 });
